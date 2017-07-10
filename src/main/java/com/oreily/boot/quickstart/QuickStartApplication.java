3dc993368258518b70a8a97de0a8d5371b264546
Alexksandr <archangel7017@gmail.com>
2017-07-10 14:39:05 +0300

    Initial commit

---
+++ src/main/java/com/oreily/boot/quickstart/QuickStartApplication.java
@@ -0,0 +1,12 @@
+package com.oreily.boot.quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickStartApplication.class, args);
+	}
+}
